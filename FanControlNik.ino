03d2cd243d67be9d47f9c9bd3cd952008f03d60d
Vykori <github@vykori.com>
2026-09-22 20:52:17 -0500

    refactor main loop

--- FanControlNik.ino
+++ FanControlNik.ino
@@ -1,12 +1,18 @@
-const int PWM_PIN = 1; // Pin 1 corresponds to PB1 (Physical Pin 6)
-const int BUTTON_PIN = 2;
-const int LED_PIN = 0;
-float min = 0.1;
+const unsigned int PWM_PIN = 1; // Pin 1 corresponds to PB1 (Physical Pin 6)
+const unsigned int BUTTON_PIN = 2;
+const unsigned int LED_PIN = 0;
+static float min = 0.1;
+static unsigned int debounce = 10; // milliseconds to ignore changes in button state
+static unsigned int pressAndHoldTime = 300; // milliseconds you have to hold the button to register as a hold, and also the miliseconds between two down-strokes to consider 2 clicks as a double-click
 float speed = 0.5;
 int dir = 1;
-
+bool wasAlreadyPressed = false;
+unsigned long buttonPressedAt = 0;
+unsigned long buttonReleasedAt = 0;
+unsigned int clicks = 0;
 unsigned long timer = 0;
 bool isHeld = 0;
+unsigned long now = 0;
 
 void setup() {
   timer = millis();
@@ -36,26 +42,43 @@ void setup() {
   setFanSpeed(speed);  // Initialize the fan to the default speed
 }
 
-void loop() {
-  setFanSpeed(speed);
-  while(digitalRead(BUTTON_PIN)==1){}; //button is not pressed 
-  delay(20); //debounce
-  while(digitalRead(BUTTON_PIN)==0){ //button is pressed
-    if(isHeld == 0){ //these things only happen once, as soon as the button is pressed
-      isHeld = 1;
-      dir *= -1;
+void loop() { // main loop interprets button presses, holds, double-clicks, etc.
+// I am also avoiding using any delays so this loop can run as many times per second as possible
+// this means debouncing needs to be taken care of with timers
+  now = millis();
+
+  if (digitalRead(BUTTON_PIN)==0 || buttonPressedAt > now - debounce) { // button is depressed (poor button, get well soon)
+    if (wasAlreadyPressed == false) { // button was just pressed on this cycle
+      wasAlreadyPressed = true;
+      // buttonPressedAt = now; // we actually need to do this 1 step later so we can use the old value for 1 more check
+      if (buttonPressedAt > now - pressAndHoldTime) { // this is a double/triple/quadruple click
+        clicks += 1;
+        buttonPressedAt = now;
+      }
+      else { // this is the first click of a new sequence
+        clicks = 1;
+        buttonPressedAt = now;
+      }
     }
-    speed += (0.01 * dir); //if button is held, increment fan
-    if(speed > 1.0){ 
-      speed = 1.0;
+    else { 
+      if (buttonPressedAt > now - pressAndHoldTime) { // button is now being held down, this should trigger something
+
+      }
     }
-    if(speed < min){
-      speed = min;
-    }
-    setFanSpeed(speed);
-    delay(50); //speed ramp delay
   }
-  isHeld = 0;
+  else if (digitalRead(BUTTON_PIN)==1 || buttonReleasedAt > now - debounce) { // button is not depressed
+    if (wasAlreadyPressed == true) { // button was just released this cycle
+      wasAlreadyPressed = false;
+      buttonReleasedAt = now;
+      if (buttonPressedAt < now - pressAndHoldTime) { // button was just released after a hold, this should trigger something
+        clicks = 0;
+      }
+    }
+    else if (buttonReleasedAt < now - pressAndHoldTime && clicks > 0) { // user has stopped clicking, this should trigger something
+      blinkLED(clicks);
+      clicks = 0;
+    }
+  }
 }
 
 void setFanSpeed(float pwm) {
